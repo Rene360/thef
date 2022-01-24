09ead1c80e6067a68617aef083f116911cceb604
faviods <fdossantos@ad-cap.com.ar>
2022-01-24 15:12:17 -0300

    Create Proyect

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./sass/main.scss";
+
+const init = () => {
+  router(window.location.hash);
+
+  window.addEventListener("hashchange", () => {
+    router(window.location.hash);
+  });
+};
+
+window.addEventListener("load", init);
